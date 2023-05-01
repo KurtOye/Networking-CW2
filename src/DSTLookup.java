c74feeaae9aa37af09f6265aa87231fd7bb96ac5
KurtOye <kurt.oyewole@city.ac.uk>
2023-05-01 11:49:46 +0100

    DSTLookup Completed

--- src/DSTLookup.java
+++ src/DSTLookup.java
@@ -6,69 +6,90 @@
 // 20384463
 // kurt.oyewole@city.ac.uk
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import org.w3c.dom.Node;
+
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.Buffer;
 import java.nio.charset.StandardCharsets;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 
 public class DSTLookup {
 
+
     // Do not change the interface!
-    public DSTLookup () {}
+    public DSTLookup() {
+    }
 
     // Do not change the interface!
     public String getValue(String startingNodeName, String key) {
-        List<String> nodeList = new ArrayList<>();
-        nodeList.add(startingNodeName);
-	// Connect to the DSTHash23 network using startingNodeName.
+        String output = "NOTFOUND";
+
+        // Connect to the DSTHash23 network using startingNodeName.
         try {
-            Socket nodeSocket = new Socket(startingNodeName, 123);
+            Socket socket = new Socket(startingNodeName, 20111);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("HELLO ephemeral");
 
-            InputStream nodeInputStream = nodeSocket.getInputStream();
-            OutputStream nodeOutputStream = nodeSocket.getOutputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String str = in.readLine();
+            System.out.println("client" + str);
 
-            nodeOutputStream.write("get_next_node_address".getBytes());
-
-            byte[] buffer = new byte[1024];
-            int bytesRead = nodeInputStream.read(buffer);
-
-            String nextNodeAddress = new String(buffer, 0, bytesRead);
-
-            while (!nextNodeAddress.isEmpty()){
-
-                Socket nextNodeSocket = new Socket(nextNodeAddress, 1234);
-
-                InputStream nextNodeInputStream = nextNodeSocket.getInputStream();
-                OutputStream nextNodeOutputStream = nextNodeSocket.getOutputStream();
-
-                buffer = new byte[1024];
-                bytesRead = nextNodeInputStream.read(buffer);
-                nextNodeAddress  = new String(buffer, 0, bytesRead);
-
-                String nextNodeName = "node-" +nodeList.size();
-                nodeList.add(nextNodeName);
-
-                nextNodeInputStream.close();
-                nextNodeOutputStream.close();
-                nextNodeSocket.close();
+            // Find the node in the network with the ID closest to the key.
+            out.println("FINDNEAREST " + key);
+            ArrayList<String> nodes = new ArrayList<>();
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                nodes.add(line);
+                System.out.println(line);
             }
+
+            if(nodes.size() < 2){
+                output = "No nearest node found for key: " + key;
+                out.close();
+                in.close();
+                socket.close();
+                return output;
+            }
+
+            for (int i = 1; i < nodes.size(); i++) {
+
+                String str1 = nodes.get(i);
+                String[] split = str1.split("/");
+                String port = split[1];
+
+                Socket nearSocket = new Socket(str1, Integer.parseInt(port));
+                PrintWriter nearOut = new PrintWriter(socket.getOutputStream(), true);
+                nearOut.println("LOOKUP" + key);
+
+                BufferedReader nearIn = new BufferedReader(new InputStreamReader(nearSocket.getInputStream()));
+                output = nearIn.readLine();
+
+                nearOut.close();
+                nearIn.close();
+                nearSocket.close();
+
+                if (!output.equals("NOTFOUND")) {
+                    break;
+                }
+            }
+
+            out.println("BYE Time-out");
+            out.close();
+            in.close();
+            socket.close();
+            return output;
+
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+        // Otherwise return "NOTFOUND"
 
-    // Find the node in the network with the ID closest to the key.
-        for (String nodename : nodeList){
-            //nodename;
-        }
-	// Use the key to get the value from the closest node.
-
-	// If the value is found, return it.
-	
-        // If the value is not found, return null.
-	return null;
     }
 }
+
